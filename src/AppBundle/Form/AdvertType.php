bfd9a4e9598c28b254b5fd37d78be47ef35dd766
ArnaudBesnard <besnard.arno@gmail.com>
2018-11-11 11:11:48 +0100

    Ajout catégories Entity + footer

--- src/AppBundle/Form/AdvertType.php
+++ src/AppBundle/Form/AdvertType.php
@@ -5,6 +5,7 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -20,11 +21,11 @@ class AdvertType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('Category', ChoiceType::class, array('label'=>'Catégorie de votre annonce','choices'  => array(
-                'Cyclisme' => "Cyclisme",
-                'Equitation' => "Equitation",
-            ),
-        ))
+        $builder->add('Category', EntityType::class, array(
+                    'label'=>'Catégorie de votre annonce',
+                    'class' => 'AppBundle:Categories',
+                    'choice_label' => 'category',
+                    ))
                 ->add('Title', TextType::class,array('label'=>'Titre de votre annonce'))
                 ->add('Content', TextareaType::class,array('label'=>'Votre annonce', 'attr' => array('rows' => 10)))
                 ->add('Price', TextType::class,array('label'=>'Prix en €'))
